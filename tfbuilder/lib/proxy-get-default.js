7c62ef9e36ae26b776aaf2b81a01ad445fb05cf9
loganpowell <loganp@tepper.cmu.edu>
2023-10-16 15:11:28 -0400

    removes tfbuilder

--- tfbuilder/lib/proxy-get-default.js
+++
@@ -1,6 +0,0 @@
-module.exports = computeDefault => function get(target, property, receiver) {
-    if (!(property in target) && property !== 'toJSON') {
-        target[property] = computeDefault(target, property, receiver);
-    }
-    return target[property];
-};
